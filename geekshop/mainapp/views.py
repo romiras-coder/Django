e89a7831d28100a23427fc002b70a55803940b24
romiras-coder <rom_iras@mail.ru>
2019-08-12 01:44:29 +0300

    начал делать меню - затрял, шаблоны подключил

--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -14,3 +14,21 @@ def contact(request):
 
 def pricelist(request):
     return render(request, 'mainapp/pricelist.html')
+
+    # links_menu = [
+    #     {'href': 'all', 'name': 'Все'},
+    #     {'href': 'k', 'name': 'КОНУСНО-ЛУЧЕВАЯ КОМПЬЮТЕРНАЯ ТОМОГРАФИЯ'},
+    #     {'href': 'c', 'name': 'ИССЛЕДОВАНИЕ ВИСОЧНО-НИЖНЕЧЕЛЮСТНЫХ СУСТАВОВ'},
+    #     {'href': 'l', 'name': 'ИССЛЕДОВАНИЕ ЛОР-ОРГАНОВ'},
+    #     {'href': 'o', 'name': 'ОРТОПАНТОМОГРАФИЯ'},
+    #     {'href': 't', 'name': 'ТЕЛЕРЕНТГЕНОГРАФИЯ'},
+    #     {'href': 'd', 'name': 'ДОПОЛНИТЕЛЬНЫЕ УСЛУГИ'},
+    #     {'href': 'kompl', 'name': 'КОМПЛЕКСНЫЕ ПРЕДЛОЖЕНИЯ'}
+    # ]
+    #
+    # content = {
+    #     'title': title,
+    #     'links_menu': links_menu,
+    #    'same_products': same_products
+    # }
+    # return render(request, 'mainapp/pricelist.html', context=content)
